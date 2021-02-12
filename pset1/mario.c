34a94038ba060ed8c4206cef7740e03820f08314
edudati <45177132+edudati@users.noreply.github.com>
2021-02-12 12:59:47 -0300

    Update mario.c

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main (void)
+int main(void)
 {
+    // Prompt to imput Height
     int n;
-
     do
-        n = get_int("Height:\n");
+    {
+        n = get_int("Height: \n");
+    }
     while (n < 1 || n > 8);
-    int nHash = n-1;
-    int nBlank = 0;
-    for (int i = 0; i < n; i++)
-        {
 
-        for (int j = 1; j < n - nBlank ; j++)
-            {
+    // Create the piramids of hashes
+    int n_hash = n - 1;
+    int n_blank = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n - n_blank ; j++)
+        {
             printf(" ");
-            }
-        for (int k = 0; k < n - nHash ; k++)
-            {
-            printf("#");
-            }
-        printf("  ");
-        for (int l = 0; l < n - nHash ; l++)
-            {
-            printf("#");
-            }
-        printf("\n");
-        nHash--;
-        nBlank++;
         }
+        for (int k = 0; k < n - n_hash ; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int l = 0; l < n - n_hash ; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        n_hash--;
+        n_blank++;
+    }
 }
